4bbef836e446043d00bb791d3d9373a8195e82c4
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-09-30 15:23:19 +0200

    implemented Log Out button logic

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    // Метод для скрытия клавиатуры тапом по экрану
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+
 }
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -32,4 +32,5 @@ final class WelcomeViewController: UIViewController {
         
         self.view.layer.insertSublayer(gradientLayer, at:0)
     }
+    
 }
