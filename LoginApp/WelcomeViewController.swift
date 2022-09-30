84b9191c962bd36eac4a1e8e1f946b7fbaf57db3
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-09-30 14:30:06 +0200

    Create UI

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Roman on 30.09.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
