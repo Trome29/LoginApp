c53fce530c685b154fdd1c91cf3d14a4c76e2a29
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-10-04 18:32:50 +0200

    first logic

---
+++ LoginApp/Models/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  LoginApp
+//
+//  Created by Roman on 04.10.2022.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let country: String
+    let city: String
+    
+    static func getUserData() -> Person {
+        Person(
+            name: "Роман",
+            surname: "Точин",
+            country: "Россия",
+            city: "Калининград"
+        )
+    }
+}
+
--- LoginApp/ViewControllers/UserDataViewController.swift
+++ LoginApp/ViewControllers/UserDataViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class UserDataViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var surnameLabel: UILabel!
+    @IBOutlet var countryLabel: UILabel!
+    @IBOutlet var cityLabel: UILabel!
+    
+    private let person = Person.getUserData()
     
     private let topColor =  UIColor(
         red: 210/255,
@@ -25,5 +31,11 @@ class UserDataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addVerticalGradientLayer(topColor: topColor, bottomColor: bottomColor)
+        
+        // Переделать
+        nameLabel.text = person.name
+        surnameLabel.text = person.surname
+        countryLabel.text = person.country
+        cityLabel.text = person.city
     }
 }
